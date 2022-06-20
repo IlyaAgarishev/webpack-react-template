398035c6e0a479980d427568edda389b40dfbc94
IlyaAgarishev <ilyagarishev@gmail.com>
2022-06-20 23:02:58 +0500

    webpack.config.js added

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const production = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: production ? "[name].[contenthash].js" : "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          production ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: !production,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: !production,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".scss"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack & React",
+      template: "./src/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({
+      filename: production ? "[name].[contenthash].css" : "[name].css",
+    }),
+  ],
+  devServer: {
+    port: 3001,
+    hot: true,
+  },
+  mode: production ? "production" : "development",
+};
